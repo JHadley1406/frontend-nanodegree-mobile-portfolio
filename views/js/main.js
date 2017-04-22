0a1b783aad94e02d280207a4cc25602ed255d0b5
Josiah Hadley <josiah.hadley@gmail.com>
2017-04-22 16:47:47 -0700

    fix: Missed a closing parentheses

--- views/js/main.js
+++ views/js/main.js
@@ -530,7 +530,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = ((i % cols) * s)+100;
-    console.log("basic left val: " + elem.basicLeft;
+    console.log("basic left val: " + elem.basicLeft);
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
